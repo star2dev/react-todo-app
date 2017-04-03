c29a1c31ac7c3b50d11ba776fd76d0a450a40193
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-03 10:49:58 +0200

    Add TodoList test

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,26 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it('should render one todo for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Walk the dog'
+    },
+    {
+      id: 2,
+      text: 'Feed the cat'
+    }];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
   });
 });
