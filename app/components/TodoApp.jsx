6078d23cc81d8d8183ba3477399901843173f748
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-04 15:15:15 +0200

    Refactored components and tests to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,8 +2,8 @@ var React = require('react');
 var uuid = require('uuid');
 var moment = require('moment');
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
@@ -17,18 +17,6 @@ var TodoApp = React.createClass({
   },
   componentDidUpdate: function () {
     TodoAPI.setTodos(this.state.todos);
-  },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined
-      }
-
-      return todo;
-    });
-
-    this.setState({todos: updatedTodos});
   },
   handleAddTodo: function (text) {
     this.setState({
@@ -62,7 +50,7 @@ var TodoApp = React.createClass({
           <div className="columns small-11 medium-6 large-5 small-centered">
             <div className="container">
               <TodoSearch onSearch={this.handleSearch}/>
-              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <TodoList/>
               <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
           </div>
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
+var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jQuery');
-var TestUtils = require('react-addons-test-utils');
 
-var TodoList = require('TodoList');
-var Todo = require('Todo');
+import {configure} from 'configureStore';
+import ConnectedTodoList, {TodoList} from 'TodoList';
+import ConnectedTodo, {Todo} from 'Todo';
 
 describe('TodoList', () => {
   it('should exist', () => {
@@ -15,15 +17,30 @@ describe('TodoList', () => {
   it('should render one todo for each todo item', () => {
     var todos = [{
       id: 1,
-      text: 'Walk the dog'
+      text: 'Walk the dog',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 500
     },
     {
       id: 2,
-      text: 'Feed the cat'
+      text: 'Feed the cat',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 500
     }];
 
-    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    var store = configure({
+      todos
+    });
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <ConnectedTodoList/>
+      </Provider>
+    );
+
+    var todoList = TestUtils.scryRenderedComponentsWithType(provider, ConnectedTodoList)[0];
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, ConnectedTodo);
 
     expect(todosComponents.length).toBe(todos.length);
   });
