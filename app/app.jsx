3fceac62bc45f1f9e39dc8ff4e939bf9761baae3
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-04 13:09:16 +0200

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Do something rad'));
+store.dispatch(actions.setSearchText('rad'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load Foundation
 $(document).foundation();
 
