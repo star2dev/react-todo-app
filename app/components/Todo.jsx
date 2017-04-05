35627e48f3e8151aa9fee4fb54d181cd5bd82181
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-05 09:31:36 +0200

    Update todos on firebase

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -21,7 +21,7 @@ export var Todo = React.createClass({
 
     return (
       <div className={todoClassname} onClick={() => {
-        dispatch(actions.toggleTodo(id));
+        dispatch(actions.startToggleTodo(id, !completed));
       }}>
         <div>
           <input type="checkbox" checked={completed}/>
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -4,19 +4,21 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var {Todo} = require('Todo');
+import * as actions from 'actions';
+import {Todo} from 'Todo';
 
 describe('Todo', () => {
   it('should exist', () => {
     expect(Todo).toExist();
   });
 
-  it('should dispatch TOGGLE_TODO action on click', () => {
+  it('should dispatch UPDATE_TODO action on click', () => {
     var todoData = {
-      id: 199,
+      id: '199',
       text: 'Test todo',
       completed: true
     };
+    var action = actions.startToggleTodo(todoData.id, !todoData.completed);
 
     var spy = expect.createSpy();
     var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
@@ -24,9 +26,6 @@ describe('Todo', () => {
     var $el = $(ReactDOM.findDOMNode(todo));
     TestUtils.Simulate.click($el[0]);
 
-    expect(spy).toHaveBeenCalledWith({
-      type: 'TOGGLE_TODO',
-      id: todoData.id
-    });
+    expect(spy).toHaveBeenCalledWith(action);
   });
 });
