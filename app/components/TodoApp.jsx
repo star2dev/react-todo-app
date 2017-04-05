e29ebf31632c954ba0f50dec84f5ee29eaecc83f
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-05 13:15:44 +0200

    Add actions for firebase login/logout

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,15 +1,27 @@
-var React = require('react');
-var uuid = require('uuid');
-var moment = require('moment');
+import React from 'react';
+import * as Redux from 'react-redux';
 
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
+import * as actions from 'actions';
 
-var TodoApp = React.createClass({
-  render: function () {
+export var TodoApp = React.createClass({
+  onLogout(e) {
+    var {dispatch} = this.props;
+    e.preventDefault();
+
+    dispatch(actions.startLogout());
+  },
+  render() {
     return (
       <div>
+        <div className="page-actions">
+          <a href="" className="hollow button alert" onClick={this.onLogout}>
+            <em className="fa fa-sign-out"></em> Logout
+          </a>
+        </div>
+
         <h1 className="page-title">Todo App</h1>
 
         <div className="row">
@@ -26,4 +38,4 @@ var TodoApp = React.createClass({
   }
 });
 
-module.exports = TodoApp;
+export default Redux.connect()(TodoApp);
--- app/firebase/index.js
+++ app/firebase/index.js
@@ -13,14 +13,6 @@ try {
 
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase;
-
-// var config = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   databaseURL: process.env.DATABASE_URL,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID
-// };
